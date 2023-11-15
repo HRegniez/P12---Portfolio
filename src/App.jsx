ae28413ad993375c0fda92e35d8d906bf08821eb
HRegniez <henriregniez124@gmail.com>
2023-11-15 21:56:42 +0100

    init vite + idea of content

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      header
+      hero
+      a propos de moi
+      Projets
+      competences
+      contact
+    </>
+  )
+}
+
+export default App
