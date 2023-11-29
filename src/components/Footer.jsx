68d1603212118127d7fef3c1914fa965ebf2e71b
HRegniez <henriregniez124@gmail.com>
2023-11-29 17:21:54 +0100

    added footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,21 @@ import '../styles/components/footer.sass'
 
 const Footer = () => {
   return (
-    <footer>
-      Footer
+    <footer className='footer'>
+      <section>
+        <h3>contact</h3>
+        <p>email: henriregniez124@gmail.com</p>
+        <p>GitHub: https://github.com/HRegniez</p>
+      </section>
+      <section>
+        <h3>About this site</h3>
+        <p>
+          This site was designed and hand-coded by myself using REACT.
+        </p>
+        <p>
+          Here is the code: https://github.com/HRegniez/P12---Portfolio
+        </p>
+      </section>
     </footer>
   )
 }
